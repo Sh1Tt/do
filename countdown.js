3e1faa4dd567bc03dc4a9b8ab4f463827802b1e9
Maurici0 <84513986+Sh1Tt@users.noreply.github.com>
2021-07-21 23:46:46 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -13,19 +13,19 @@ const countdown =
   },
   state:
   {
-    Off: `Countdown start: 1st of July ${__d.getFullYear() + 1}`
+    off: `Countdown start: 1st of July ${__d.getFullYear() + 1}`
   },
   target:
   {
-    day: 24,
-    month: 7
+    day: 24 - 1,
+    month: 7 - 1
   }
 };
 function updateCounter()
 {
   const dayToGo = () => countdown.target.day - __d.getDate();
   const hourToGo = () => dayToGo() * 24;
-  countdown.display.innerText = ( ( ( __d.getMonth() + 1 ) === countdown.target.month ) && ( __d.getDate() + 1 < countdown.target.day ) ) ? `${23 - __d.getHours() + hourToGo()} : ${59 - __d.getMinutes()} : ${59 - __d.getSeconds()}` : countdown.state.Off;
+  countdown.display.innerText = ( ( __d.getMonth() !== countdown.target.month ) || ( __d.getDate() > countdown.target.day ) ) ? countdown.state.off : `${23 - __d.getHours() + hourToGo()} : ${59 - __d.getMinutes()} : ${59 - __d.getSeconds()}`;
   countdown.step();
 }
 updateCounter();
