16ba89bba09cc264bc2a0f2dbb12a5925f8a184a
Maurici0 <84513986+Sh1Tt@users.noreply.github.com>
2021-07-22 00:41:52 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -23,13 +23,13 @@ const countdown =
 function updateCounter()
 {
   const __d = new Date();
-  const doubleDigits = n => ( n.length == 1 ? `0${n}` : n );
+  const doubleDigits = n => ( n.length == 1 ? `0${n}` : `${n}` );
   const daysBefore = countdown.target.day - __d.getDate();
   const toGo = 
   {
-    Hours: () => doubleDigits( ( daysBefore * 24 ) + ( 23 - __d.getHours() ) ),
-    Minutes: () => doubleDigits( 59 - __d.getMinutes() ),
-    Seconds: () => doubleDigits( 59 - __d.getSeconds() )
+    Hours: () => ( daysBefore * 24 ) + ( 23 - __d.toLocaleTimeString('nl-NL', { hour: "2-digit" } ) ),
+    Minutes: () => __d.toLocaleTimeString('nl-NL', { minute: "2-digit" } ),
+    Seconds: () => __d.toLocaleTimeString('nl-NL', { second: "2-digit" } )
   };
   countdown.display.innerText = ( ( __d.getMonth() !== countdown.target.month ) || ( __d.getDate() > countdown.target.day ) ) ? countdown.state.off : `${toGo.Hours()} : ${toGo.Minutes()} : ${doubleDigits( 59 - __d.getSeconds() )}`;
   countdown.step();
