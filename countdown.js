ca70dfb7bfd12839699f8ce4056d3590f675f364
Maurici0 <84513986+Sh1Tt@users.noreply.github.com>
2021-07-22 00:44:18 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -31,7 +31,7 @@ function updateCounter()
     Minutes: () => __d.toLocaleTimeString('nl-NL', { minute: "2-digit" } ),
     Seconds: () => __d.toLocaleTimeString('nl-NL', { second: "2-digit" } )
   };
-  countdown.display.innerText = ( ( __d.getMonth() !== countdown.target.month ) || ( __d.getDate() > countdown.target.day ) ) ? countdown.state.off : `${toGo.Hours()} : ${toGo.Minutes()} : ${doubleDigits( 59 - __d.getSeconds() )}`;
+  countdown.display.innerText = ( ( __d.getMonth() !== countdown.target.month ) || ( __d.getDate() > countdown.target.day ) ) ? countdown.state.off : `${toGo.Hours()} : ${toGo.Minutes()} : ${toGo.Seconds()}`;
   countdown.step();
 }
 updateCounter();
