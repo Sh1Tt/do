ceff65e004b8831430e67de6f78c41b0179be081
Maurici0 <84513986+Sh1Tt@users.noreply.github.com>
2021-07-21 23:06:47 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -9,6 +9,10 @@ const countdown =
     }, 
     100
     );  
+  },
+  state:
+  {
+    Off: `Countdown will start again July ${new Date().getYear() + 1}`
   }
 };
 function updateCounter()
@@ -19,9 +23,11 @@ function updateCounter()
     day: 24 - 1,
     month: 7
   };
+  
+  
   const dayToGo = () => _birth.day - __d.getDate();
   const hourToGo = () => dayToGo() * 24;
-  countdown.display.innerText = `${23 - __d.getHours() + hourToGo()} : ${59 - __d.getMinutes()} : ${59 - __d.getSeconds()}`;
+  countdown.display.innerText = ( ( ( __d.getMonth() + 1 ) !== _birth.month ) || ( __d.getDate() >= _birth.day ) ) ? countdown.state.Off : `${23 - __d.getHours() + hourToGo()} : ${59 - __d.getMinutes()} : ${59 - __d.getSeconds()}`;
   countdown.step();
 }
 updateCounter();
