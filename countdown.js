ba74dd8ddcb44e64a1f962a507eb928fc77621cd
Maurici0 <84513986+Sh1Tt@users.noreply.github.com>
2021-07-22 00:03:08 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -25,7 +25,7 @@ function updateCounter()
   const __d = new Date();
   const dayToGo = () => countdown.target.day - __d.getDate();
   const hourToGo = () => dayToGo() * 24;
-  const doubleDigits = n => '0' + n.slice( -2 );
+  const doubleDigits = n => ( n.length == 1 ? `0${n}` : n )
   countdown.display.innerText = ( ( __d.getMonth() !== countdown.target.month ) || ( __d.getDate() > countdown.target.day ) ) ? countdown.state.off : `${doubleDigits( ( 23 - __d.getHours() ) + hourToGo() )} : ${doubleDigits( 59 - __d.getMinutes() )} : ${doubleDigits( 59 - __d.getSeconds() )}`;
   countdown.step();
 }
